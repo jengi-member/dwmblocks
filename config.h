ccd41e27cc9d971967c1646abe39dd976878fb2b
Void User <mathias12345cson@gmail.com>
2023-07-20 21:07:05 +0300

    first commit

---
+++ config.h
@@ -0,0 +1,18 @@
+#define CMDLENGTH 200
+#define DELIMITER "  "
+#define LEADING_DELIMITER
+//#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	{"capslock", 0, 5},
+	{"xbpsupdates.sh", 1200, 3},
+	{"disk.sh", 15, 0},
+	{"memory.sh", 5, 0},
+	{"swap.sh", 5, 0},
+	{"temperature.sh",   5,  0},
+	{"internet.sh", 5, 0},
+	{"volume.sh",  0,  1},
+	{"battery.sh", 3, 0},
+	{"date.sh",	0,		4},
+	{"clock.sh",					0,		2},
+};
